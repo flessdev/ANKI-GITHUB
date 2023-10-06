462502563907a76af83f461e16f4f6373e5d4094
flessdev <livinginthesunlight72@protonmail.com>
2023-10-06 13:19:30 -0500

    first commit

---
+++ prog_Editor/front/_utils.js
@@ -0,0 +1,232 @@
+function copyToClipboard(text) {
+  let temp = document.createElement('textarea');
+  temp.value = text;
+  document.body.append(temp);
+  temp.select();
+  document.execCommand('copy');
+  document.body.removeChild(temp);
+}
+
+
+function beauty(code) {
+  return code?.trim() ?? ''
+}
+Object.defineProperty(Object.prototype, 'map', {
+  value: function (callback) {
+    const result = {};
+    for (const key in this) {
+      if (this.hasOwnProperty(key)) {
+        result[key] = callback(this[key], key);
+      }
+    }
+    return result;
+  },
+});
+
+function separateCode(code) {
+  const html = code.replace(/<(style|script)>.*?<\/\1>/gis,
+    "")
+  const css = code.match(/(?<=<(style)>).*?(?=<\/\1>)/gis,
+    "")?.join("\n");
+  const js = code.match(/(?<=<(script)>).*?(?=<\/\1>)/gis,
+    "")?.join("\n");
+
+  return {
+    html, css, js
+  }.map(beauty)
+}
+
+function sortByLength(code) {
+  const CODE = separateCode(code);
+  const keysWithContent = Object.keys(CODE).filter(key => CODE[key].length > 0);
+  keysWithContent.sort((a, b) => CODE[b].length - CODE[a].length);
+
+  return keysWithContent;
+
+}
+
+/*function addSwipeListener(options) {
+    const { distance = 100, callback } = options;
+    let startX = 0;
+    function handleTouchStart(event) {
+      startX = event.touches[0].clientX;
+    }
+    function handleTouchEnd(event) {
+      const endX = event.changedTouches[0].clientX;
+      const swipeDistance = endX - startX;
+      if (Math.abs(swipeDistance) >= distance) {
+        const swipeDirection = (swipeDistance > 0) ? 'left' : 'right';
+        callback(swipeDirection);
+      }
+    }
+    document.documentElement.addEventListener('touchstart', handleTouchStart);
+    document.documentElement.addEventListener('touchend', handleTouchEnd);
+  }*/
+
+function addSwipeListener(options) {
+  const {
+    distance = 80, minSpeed = 0.5, callback
+  } = options;
+  let startX = 0;
+  let startTime = 0;
+
+  function handleTouchStart(event) {
+    startX = event.touches[0].clientX;
+    startTime = Date.now();
+  }
+
+  function handleTouchEnd(event) {
+    const endX = event.changedTouches[0].clientX;
+    const swipeDistance = endX - startX;
+    const endTime = Date.now();
+
+    const timeDiff = endTime - startTime;
+
+    // Calcula la velocidad en píxeles por milisegundo
+    const swipeSpeed = Math.abs(swipeDistance) / timeDiff;
+
+    if (Math.abs(swipeDistance) >= distance && swipeSpeed >= minSpeed) {
+      const swipeDirection = (swipeDistance > 0) ? 'left': 'right';
+      callback(swipeDirection);
+    }
+  }
+
+  document.documentElement.addEventListener('touchstart', handleTouchStart);
+  document.documentElement.addEventListener('touchend', handleTouchEnd);
+}
+
+// Uso de la función con un umbral de velocidad mínimo
+
+
+function obtenerIndiceStringMasLargo(strings) {
+  let indiceMasLargo = 0;
+  let longitudMasLarga = 0;
+
+  for (let i = 0; i < strings.length; i++) {
+    if (strings[i].length > longitudMasLarga) {
+      longitudMasLarga = strings[i].length;
+      indiceMasLargo = i;
+    }
+  }
+
+  return indiceMasLargo;
+}
+
+
+const createHtml = ({
+  css, html, js
+}) => {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <style id="preview-style">
+  ${css}
+  </style>
+  <script>
+  function logToConsole(message){
+  setTimeout(_=> window.parent.postMessage(message, "*") , 500)
+
+  }
+  for (const key of Object.keys(console)) {
+  window.console[key] = function(...args) {
+  const logArgs = args.map(arg => typeof arg === "object" ? JSON.stringify(arg) : arg);
+  logToConsole(key + ": " + logArgs.join(" "));
+  };
+  }
+  </script>
+  </head>
+  <body>
+  ${html}
+  <script>
+  ${js}
+  </script>
+  </body>
+  </html>`
+}
+
+
+let previewUrl = null;
+
+function getPreviewUrl() {
+  return previewUrl
+}
+
+function updatePreview( {
+  html, css, js
+}) {
+  if (previewUrl) {
+    URL.revokeObjectURL(previewUrl)
+  }
+
+  const htmlForPreview = createHtml({
+    html, css, js
+  })
+
+  const blob = new window.Blob([htmlForPreview], {
+    type: 'text/html'
+  })
+
+  previewUrl = URL.createObjectURL(blob)
+
+  /* if (previewWindowRef?.deref()) {
+     previewWindowRef.deref().location = previewUrl
+   }*/
+}
+
+function generateIdentifier(inputString) {
+  const hash = inputString.hashCode(); // Usamos la función hashCode() como ejemplo
+  const identifier = String(hash)//.slice(0, 4); // Tomamos los primeros 3 dígitos del hash
+
+  return identifier;
+}
+
+String.prototype.hashCode = function () {
+  let hash = 0;
+  if (this.length === 0) return hash;
+
+  for (let i = 0; i < this.length; i++) {
+    const char = this.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash &= hash; // Convierte a un valor entero de 32 bits
+  }
+
+  return hash;
+};
+
+
+function limpiarLocalStorage(claveUltimaLimpieza, tiempoLimpiezaEnDias) {
+  const now = new Date().getTime();
+  const tiempoLimpiezaEnMilisegundos = tiempoLimpiezaEnDias * 24 * 60 * 60 * 1000; // Convertir días a milisegundos
+
+  // Obtenemos la fecha de la última limpieza (si existe)
+  const ultimaLimpieza = localStorage.getItem(claveUltimaLimpieza);
+
+  // Verificamos si no existe registro de la última limpieza
+  // o si ha pasado más de tiempoLimpiezaEnMilisegundos desde la última limpieza
+  if (!ultimaLimpieza || (now - parseInt(ultimaLimpieza)) >= tiempoLimpiezaEnMilisegundos) {
+    // Realizamos la limpieza de datos
+    for (let i = 0; i < localStorage.length; i++) {
+      const clave = localStorage.key(i);
+      const datos = JSON.parse(localStorage.getItem(clave));
+
+      if (datos && datos.fechaAgregado && (now - datos.fechaAgregado) >= tiempoLimpiezaEnMilisegundos) {
+        // Los datos son más antiguos de tiempoLimpiezaEnMilisegundos, así que los eliminamos
+        localStorage.removeItem(clave);
+      }
+    }
+
+    // Guardamos la nueva fecha de la última limpieza
+    localStorage.setItem(claveUltimaLimpieza, now.toString());
+  }
+}
+
+function agregarItemConFecha(key, value) {
+  const now = new Date().getTime(); // Obtenemos la fecha actual en milisegundos
+  const data = {
+    value,
+    date: now
+  };
+  localStorage.setItem(key, JSON.stringify(data));
+}
---
+++ prog_Editor/front/script.js
@@ -0,0 +1,200 @@
+ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6')
+const hashCode = generateIdentifier(good_code)
+const identifier = hashCode;
+const retrievedData = JSON.parse(localStorage.getItem(identifier));
+console.log(retrievedData)
+const badCodeObj = retrievedData ? JSON.parse(retrievedData.value): separateCode(bad_code);
+
+enabledLanguages = enabledLanguages
+? enabledLanguages.split(/\s+/): good_code
+? sortByLength(good_code): ["html",
+  "css",
+  "js"];
+console.log(typeof badCodeObj)
+
+
+$(".instructions").text(instructions)
+//$('#console').dragable()
+
+limpiarLocalStorage('ultimaLimpieza', 1);
+
+window.onload = () => {
+  update()
+  $goodIframe.attr('srcdoc', good_code);
+}
+
+const $badIframe = $('.front iframe')
+const $goodIframe = $('.back iframe');
+
+const $pretiffy = $('.prettify')
+//const $changeBtn = $('.change');
+const $banner = $('.banner')
+const $saveBtn = $('button.save');
+console.log('test1')
+
+
+
+$saveBtn.click(_ => {
+  let a = group1['html']?.getValue().trim();
+  let b = group1['css']?.getValue().trim();
+  let c = group1['js']?.getValue().trim();
+
+  a = a ? a: false;
+  b = b ? `<style>\n${b}\n<\/style>`: false;
+  c = c ? `<script>\n${c}\n<\/script>`: false;
+  copyToClipboard([a, b, c].filter(e => e).join('\n'))
+})
+
+//👇 Funcion de Teke
+
+
+
+
+
+
+
+
+async function getBeatify(editor) {
+  await import ("https://cdnjs.cloudflare.com/ajax/libs/ace/1.25.1/ext-beautify.min.js");
+  const beautify = ace.require("ace/ext/beautify");
+  $pretiffy.click(_ => {
+    beautify.beautify(editor.session)
+  })
+}
+
+
+class createGroup {
+  constructor(selector, codeObj, iframe) {
+    this.CODE = codeObj//separateCode(code);
+
+    this.editor = ace.edit(selector, {
+      theme: 'ace/theme/monokai',
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      printMargin: false,
+      scrollPastEnd: 1,
+    })
+
+    //LINTER
+    //const provider = LanguageProvider.fromCdn("https://www.unpkg.com/ace-linters@latest/build/");
+    //provider.registerEditor(this.editor);
+    console.log('test2')
+    enabledLanguages.forEach((b, c) => {
+      const lang = (b == 'js') ? 'javascript': b;
+      this[b] = ace.createEditSession(this.CODE[b]);
+      console.log('4')
+      this[b].setOptions({
+        tabSize: 1,
+        mode: `ace/mode/${lang}`,
+        wrap: true,
+
+      })
+      console.log("test3")
+    })
+
+
+    this.editor.on('change', update);
+
+
+    this.getValues = function() {
+      const values = {
+        html: this['html']?.getValue() ?? this.CODE.html,
+        css: this['css']?.getValue() ?? this.CODE.css,
+        js: this['js']?.getValue() ?? this.CODE.js
+      }
+      return createHtml(values)
+    }
+  }
+}
+
+
+const group1 = new createGroup($('.front .editor').get(0), badCodeObj, $badIframe)
+getBeatify(group1.editor)
+let index = 0;
+
+const DIRECTIONS = {
+  right: (r, i) => ++i % r,
+  left: (r, i) => (--i + r) % r,
+  undefined: (r, i) => i
+}
+
+function changer(direction) {
+  const range = enabledLanguages.length;
+  index = DIRECTIONS[direction](range, index)
+  $(".front").attr('language', enabledLanguages[index])
+
+  group1.editor.setSession(group1[enabledLanguages[index]])
+}
+
+$(".front").attr('language', enabledLanguages[index])
+group1.editor.setSession(group1[enabledLanguages[index]])
+
+addSwipeListener({
+  distance: 70,
+  callback: d => changer(d)
+});
+
+$banner.click(function() {
+  //alert(keyboard.isOpen())
+  if (keyboard.isOpen()) group1.editor.focus()
+})
+
+
+
+$(window).resize(_ => {
+  $('.instructions').addClass('compress');
+});
+
+
+$('.instructions').click(function() {
+  $(this).toggleClass('compress')
+})
+
+$('.mostrar').on('touchstart',
+  _ => {
+    $goodIframe.toggleClass('zindex');
+  });
+
+
+
+
+
+
+$('.update').click(_ => {
+  $goodIframe.attr('srcdoc', good_code);
+  update()
+})
+
+document.addEventListener('DOMContentLoaded',
+  () => {
+
+    window.addEventListener("message", function(event) {
+      if (event.source === $badIframe.get(0).contentWindow) {
+        $("#console")[0].textContent += event.data + '\n';
+      }
+    });
+
+  })
+
+let interval = null;
+
+function update(language, code) {
+  if (interval) clearInterval(interval)
+  $("#console").text('')
+
+
+  //updatePreview(page)
+  //$iframe.src =  getPreviewUrl()
+
+  interval = setTimeout(_ => {
+    $badIframe.attr('srcdoc', group1.getValues());
+    const data = {
+      html: group1.html?.getValue() ?? "",
+      css: group1.css?.getValue() ?? "",
+      js: group1.js?.getValue() ?? ""
+    }
+
+
+    agregarItemConFecha(identifier, JSON.stringify(data))
+  }, 500)
+}
